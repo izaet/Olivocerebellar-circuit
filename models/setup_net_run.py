edd64275cefb63f712a3bb32f0c461a07f0d4f5a
izaet <izetdewit@gmail.com>
2026-09-05 16:32:23 +0200

    Sync for cluster

--- models/setup_net_run.py
+++ models/setup_net_run.py
@@ -36,9 +36,9 @@ def init_net_and_runner(net_params=None, dt=0.025 , seed=88, jit=True):
 
     # --- Monitors Configuration --- #
     monitor_function = monitor_presets[net_params["monitor_preset"]]
-    monitors = {**monitor_function(net)}
+    monitors = monitor_function(net)
     
-    runner = bp.DSRunner(net, monitors=monitors, dt=dt, jit=jit, progress_bar=False, memory_efficient=True)
+    runner = bp.DSRunner(net, monitors=monitors, dt=dt, jit=jit, progress_bar=False)
     if jit:
         runner._fun_predict = bm.jit(runner._fun_predict)
     return net, runner
@@ -131,9 +131,9 @@ def init_and_run(duration=1000.0, dt=0.025, net_params=None, seed=42, jit=True):
     # --- Monitors Configuration --- #
 
     monitor_function = monitor_presets[net_params["monitor_preset"]]
-    monitors = {**monitor_function(net)}
+    monitors = monitor_function(net)
 
-    runner = bp.DSRunner(net, monitors=monitors, dt=dt, jit =jit, progress_bar=True, memory_efficient=True)
+    runner = bp.DSRunner(net, monitors=monitors, dt=dt, jit =jit, progress_bar=True)
     runner.progress_bar = False
     if jit:
         runner._fun_predict = bm.jit(runner._fun_predict)
