6850d297fadc24daa81d94c97f2abe0bbb7a16c1
izaet <izetdewit@gmail.com>
2026-09-05 16:19:02 +0200

    monitor fix

--- models/setup_net_run.py
+++ models/setup_net_run.py
@@ -36,7 +36,7 @@ def init_net_and_runner(net_params=None, dt=0.025 , seed=88, jit=True):
 
     # --- Monitors Configuration --- #
     monitor_function = monitor_presets[net_params["monitor_preset"]]
-    monitors = monitor_function(net)
+    monitors = {**monitor_function(net)}
     
     runner = bp.DSRunner(net, monitors=monitors, dt=dt, jit=jit, progress_bar=False, memory_efficient=True)
     if jit:
@@ -131,7 +131,7 @@ def init_and_run(duration=1000.0, dt=0.025, net_params=None, seed=42, jit=True):
     # --- Monitors Configuration --- #
 
     monitor_function = monitor_presets[net_params["monitor_preset"]]
-    monitors = monitor_function(net)
+    monitors = {**monitor_function(net)}
 
     runner = bp.DSRunner(net, monitors=monitors, dt=dt, jit =jit, progress_bar=True, memory_efficient=True)
     runner.progress_bar = False
